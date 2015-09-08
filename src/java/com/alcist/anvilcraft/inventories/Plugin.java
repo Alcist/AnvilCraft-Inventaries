ae540227ef315e57d790aa1020058d8981a8a58e
alcaamado <alcaamado224@gmail.com>
2015-09-08 22:22:55 +0200

    Project refactor

---
+++ src/java/com/alcist/anvilcraft/inventories/Plugin.java
@@ -0,0 +1,18 @@
+package com.alcist.anvilcraft.inventories;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+}
